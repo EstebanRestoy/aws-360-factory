726349008a3854b93a0dedcae755e8809ffd3c96
EstebanRestoy <55629825+EstebanRestoy@users.noreply.github.com>
2022-10-18 16:55:40 +0200

    Update create-ec2.sh

--- 2_awscli/create-ec2.sh
+++ 2_awscli/create-ec2.sh
@@ -11,7 +11,13 @@ set -e
 # Get the AMI_ID
 ######################################################################
 
+
 echo "Getting AMI Id..."
+
+EC2_AMI_ID=$(aws ec2 describe-images --output text \
+--owners amazon \
+--filters 'Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*' \
+--query 'reverse(sort_by(Images[], &CreationDate))[0].ImageId')
 
 # TODO
 
@@ -23,6 +29,11 @@ echo "Got AMI Id: ${EC2_AMI_ID}"
 
 echo "Getting VPC Id..."
 
+EC2_VPC_ID=$(aws ec2 describe-vpcs \
+--filters 'Name=is-default,Values=true' \
+--query 'Vpcs[0].VpcId' \
+--output text)
+
 ### TODO
 
 echo "Got VPC Id: ${EC2_VPC_ID}"
@@ -33,6 +44,7 @@ echo "Got VPC Id: ${EC2_VPC_ID}"
 
 echo "Getting Subnet Id..."
 
+EC2_SUBNET_ID=$(aws ec2 describe-subnets --filter "Name=vpc-id,Values=${EC2_VPC_ID}" --query 'Subnets[0].SubnetId' --output text)
 ### TODO
 
 echo "Got Subnet Id: ${EC2_SUBNET_ID}"
@@ -44,6 +56,7 @@ echo "Got Subnet Id: ${EC2_SUBNET_ID}"
 echo "Getting Security Group Id..."
 
 ### TODO
+EC2_SG_ID=$(aws ec2 describe-security-groups --filter "Name=vpc-id,Values=${EC2_VPC_ID}" --query 'SecurityGroups[0].GroupId' --output text)
 
 echo "Got Security Group Id: ${EC2_SG_ID}"
 
@@ -54,6 +67,8 @@ echo "Got Security Group Id: ${EC2_SG_ID}"
 echo "Provisioning EC2 instance..."
 
 ### TODO
+
+EC2_PROVISION=$(aws ec2 run-instances --image-id ${EC2_AMI_ID} --count 1 --instance-type t2.micro --key-name vockey --security-group-ids ${EC2_SG_ID} --subnet-id ${EC2_SUBNET_ID})
 
 echo "Ec2 Instance ready, here are the details:"
 echo ${EC2_PROVISION}
